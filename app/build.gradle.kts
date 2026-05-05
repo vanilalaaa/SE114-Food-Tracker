af5110793f4afcf64a6b51b8fa4bfc1a1cf86ba2
Nguyen Pham Thuy Vy <24522059@gm.uit.edu.vn>
2026-05-05 20:21:26 +0700

    chore: add material-icons-extended dependency via version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation("androidx.compose.material:material-icons-extended")
+    implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.foundation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
