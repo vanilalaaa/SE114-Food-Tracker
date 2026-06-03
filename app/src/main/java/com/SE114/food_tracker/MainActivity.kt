2c9d5f66e28a3d66713ca75e87c873bb7b865af2
Huynh Tinh Van <htvan.se@gmail.com>
2026-06-03 19:46:11 +0700

    feat(core): add navigation + main scaffold

--- app/src/main/java/com/SE114/food_tracker/MainActivity.kt
+++ app/src/main/java/com/SE114/food_tracker/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.SE114.food_tracker.core.MainScaffold
 import com.SE114.food_tracker.ui.theme.FoodTrackerTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,29 +15,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FoodTrackerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScaffold()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FoodTrackerTheme {
-        Greeting("Android")
     }
 }
