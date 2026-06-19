3841a17ce67a7ff6383d31ed9e574427c3146e65
ThaoUyen <ht.thaouyen1203@gmail.com>
2026-06-19 17:26:04 +0700

    feat: Thêm tab chat vào bottombar

--- app/src/main/java/com/SE114/food_tracker/core/designsystem/components/BottomBar.kt
+++ app/src/main/java/com/SE114/food_tracker/core/designsystem/components/BottomBar.kt
@@ -4,8 +4,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MenuBook
+import androidx.compose.material.icons.outlined.Article
 import androidx.compose.material.icons.outlined.BarChart
-import androidx.compose.material.icons.outlined.Payments
+import androidx.compose.material.icons.outlined.Forum
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
@@ -25,7 +26,8 @@ fun BottomBar(selectedIndex: Int = 0, onItemSelected: (Int) -> Unit) {
         val icons = listOf(
             Icons.Filled.MenuBook,
             Icons.Outlined.BarChart,
-            Icons.Outlined.Payments,
+            Icons.Outlined.Article,
+            Icons.Outlined.Forum,
             Icons.Outlined.Settings
         )
         icons.forEachIndexed { index, icon ->
